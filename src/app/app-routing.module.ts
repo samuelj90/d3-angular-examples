62f47a1408d40c3ad9a1e51cd86c49b3fec1977b
Samuel J Mathew <samuelj90@gmail.com>
2023-04-02 12:00:36 +0400

    Fixing the routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent, loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    loadChildren: () =>
+      import('./feature/feature.module').then((m) => m.FeatureModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
